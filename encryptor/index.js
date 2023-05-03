e4326018a08cf8359a85ec7212f999bb12305cc8
Brian Hart <hart.briant@gmail.com>
2023-05-03 13:17:05 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,1 +1,8 @@
 console.log('The encryptor js file loaded'); 
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+for (let i = 0; i < letters.length; i++) {
+  console.log(letters[i]); 
+}

217343a48ae11f07f864a48f7934d4cf78e88ca2
Brian Hart <hart.briant@gmail.com>
2023-05-10 12:58:11 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -3,6 +3,26 @@ console.log('The encryptor js file loaded');
 const letters = 'abcdefghijklmnopqrstuvwxyz';
 const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
 
-for (let i = 0; i < letters.length; i++) {
-  console.log(letters[i]); 
+function encrypt(text) {
+  let result = '';
+  
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i];
+    let letterIndex = letters.indexOf(letter);
+    
+    if (letterIndex < 0) {
+      result += letter;
+    }
+    
+    else {
+      result += encrypt_key[letterIndex]; 
+    }
+  }
+  
+  return result;
+  
 }
+
+encrypt('hello!');
+
+
